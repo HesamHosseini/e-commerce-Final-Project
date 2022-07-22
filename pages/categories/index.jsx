65ed90482e978ec6d34ff10689a66cd37eb1b3af
HesamHosseini <hesihesi81.hosseini@gmail.com>
2022-07-22 17:15:01 +0430

    dev branch created

--- pages/categories/index.jsx
+++ pages/categories/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 function categorySummery(props) {
-  return <div>this is categorySummery file</div>;
+  return <div>
+    this is categorySummery file</div>;
 }
 
 export default categorySummery;
